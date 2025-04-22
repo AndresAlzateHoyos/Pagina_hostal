60c56534d45a326538254af8eb06cd34aa1c1127
AndresAlzate <andresalzate.trad@gmail.com>
2025-04-21 20:34:12 -0500

    Add events, create styles for events and create json with info for the events

--- src/events/Events.jsx
+++ src/events/Events.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import styles from './Events.module.css';
+import events from './events.json';
 
 export default function Events () {
     return (
         <main>
-            <h1>Events</h1>
-            <div>Aquí van los eventos</div>
+            <h1>Weekle Events at The Wandering Paisa Hostel</h1>
+            <div className={styles.allEvents}>
+                {events.map(event => (
+                    <div className={styles.singleEvent}>
+                        <h3>{event.name} - {event.day}, {event.timeStart} - {event.timeEnd}</h3>
+                        <p>{event.info}</p>
+                        <div className={styles.eventImg}><img src={event.image}></img></div>
+                    </div>
+                ))}
+            </div>
         </main>
     )
 }
