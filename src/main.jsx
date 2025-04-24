2287e8bc9deffd401ed5b6701cad01a12e9bf019
AndresAlzateHoyos <andresalzate.trad@gmail.com>
2025-04-24 09:35:47 -0500

    Change BrowserRouter for HashRouter to improve deployment on github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
      <App />
-  </BrowserRouter>
+  </HashRouter>
 )
