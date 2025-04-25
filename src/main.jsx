e8f6939fa9646fee8245bc2d0c674986b671f55b
AndresAlzateHoyos <andresalzate.trad@gmail.com>
2025-04-25 07:29:03 -0500

    Styles damaged while trying to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import App from './app/App.jsx';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './styles.css';
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
      <App />
-  </HashRouter>
+  </BrowserRouter>
 )
